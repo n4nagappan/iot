0cff3aa7c367b383b255009dabe1dd1f29040a5e
Arrchana <arrchu@gmail.com>
2015-04-23 18:15:12 +0800

    sending GPS data to server

--- iotSketch/iotSketch.ino
+++ iotSketch/iotSketch.ino
@@ -17,20 +17,138 @@
 #include <LTask.h>
 #include <LWiFi.h>
 #include <LWiFiClient.h>
+#include <LGPS.h>
 
 #define WIFI_AP "AndroidAP"
 #define WIFI_PASSWORD "nuscfast"
 #define WIFI_AUTH LWIFI_WPA  // choose from LWIFI_OPEN, LWIFI_WPA, or LWIFI_WEP.
-#define SITE_URL "www.b4b0a9ed.ngrok.io"
+#define SITE_URL "www.722b4490.ngrok.io"
 
 LWiFiClient c;
 
+/*GPS info variables*/
+gpsSentenceInfoStruct info;
+char buff[256];
+double latitude;
+double longitude;
+double latno, latdegree, longno, longdegree;
+
+static unsigned char getComma(unsigned char num,const char *str)
+{
+  unsigned char i,j = 0;
+  int len=strlen(str);
+  for(i = 0;i < len;i ++)
+  {
+     if(str[i] == ',')
+      j++;
+     if(j == num)
+      return i + 1; 
+  }
+  return 0; 
+}
+
+static double getDoubleNumber(const char *s)
+{
+  char buf[10];
+  unsigned char i;
+  double rev;
+  
+  i=getComma(1, s);
+  i = i - 1;
+  strncpy(buf, s, i);
+  buf[i] = 0;
+  rev=atof(buf);
+  return rev; 
+}
+
+static double getIntNumber(const char *s)
+{
+  char buf[10];
+  unsigned char i;
+  double rev;
+  
+  i=getComma(1, s);
+  i = i - 1;
+  strncpy(buf, s, i);
+  buf[i] = 0;
+  rev=atoi(buf);
+  return rev; 
+}
+
+void parseGPGGA(const char* GPGGAstr)
+{
+  /* Refer to http://www.gpsinformation.org/dale/nmea.htm#GGA
+   * Sample data: $GPGGA,123519,4807.038,N,01131.000,E,1,08,0.9,545.4,M,46.9,M,,*47
+   * Where:
+   *  GGA          Global Positioning System Fix Data
+   *  123519       Fix taken at 12:35:19 UTC
+   *  4807.038,N   Latitude 48 deg 07.038' N
+   *  01131.000,E  Longitude 11 deg 31.000' E
+   *  1            Fix quality: 0 = invalid
+   *                            1 = GPS fix (SPS)
+   *                            2 = DGPS fix
+   *                            3 = PPS fix
+   *                            4 = Real Time Kinematic
+   *                            5 = Float RTK
+   *                            6 = estimated (dead reckoning) (2.3 feature)
+   *                            7 = Manual input mode
+   *                            8 = Simulation mode
+   *  08           Number of satellites being tracked
+   *  0.9          Horizontal dilution of position
+   *  545.4,M      Altitude, Meters, above mean sea level
+   *  46.9,M       Height of geoid (mean sea level) above WGS84
+   *                   ellipsoid
+   *  (empty field) time in seconds since last DGPS update
+   *  (empty field) DGPS station ID number
+   *  *47          the checksum data, always begins with *
+   */
+  int tmp, hour, minute, second, num ;
+  Serial.println(GPGGAstr);
+  if(GPGGAstr[0] == '$')
+  {
+    tmp = getComma(1, GPGGAstr);
+    hour     = (GPGGAstr[tmp + 0] - '0') * 10 + (GPGGAstr[tmp + 1] - '0');
+    minute   = (GPGGAstr[tmp + 2] - '0') * 10 + (GPGGAstr[tmp + 3] - '0');
+    second    = (GPGGAstr[tmp + 4] - '0') * 10 + (GPGGAstr[tmp + 5] - '0');
+    
+    sprintf(buff, "UTC timer %2d-%2d-%2d", hour, minute, second);
+    Serial.println(buff);
+    
+    tmp = getComma(2, GPGGAstr);
+    latitude = getDoubleNumber(&GPGGAstr[tmp]);
+    latno = latitude/100;
+    latdegree = latitude - latno;
+    tmp = getComma(4, GPGGAstr);
+    longitude = getDoubleNumber(&GPGGAstr[tmp]);
+    longno = longitude/100;
+    longdegree = longitude - longno;
+    sprintf(buff, "latitude = %10.4f, longitude = %10.4f", latitude, longitude);
+    Serial.println(buff); 
+    
+    tmp = getComma(7, GPGGAstr);
+    num = getIntNumber(&GPGGAstr[tmp]);    
+    sprintf(buff, "satellites number = %d", num);
+    Serial.println(buff); 
+  }
+  else
+  {
+    Serial.println("Not get data"); 
+  }
+}
+
 void setup()
 {
+  /*Power on WiFi module*/
   LTask.begin();
   LWiFi.begin();
+  
   Serial.begin(115200);
-
+  
+  /*Power on GPS module*/
+  LGPS.powerOn();
+  Serial.println("LGPS Power on, and waiting ..."); 
+  delay(3000);
+  
   // keep retrying until connected to AP
   Serial.println("Connecting to AP");
   while (0 == LWiFi.connect(WIFI_AP, LWiFiLoginInfo(WIFI_AUTH, WIFI_PASSWORD)))
@@ -41,6 +159,23 @@ void setup()
 }
 
 boolean disconnectedMsg = false;
+
+//Rounds down (via intermediary integer conversion truncation)
+String doubleToString(double input,int decimalPlaces){
+  if(decimalPlaces!=0){
+  String string = String((int)(input*pow(10,decimalPlaces)));
+  if(abs(input)<1){
+    if(input>0)
+      string = "0"+string;
+    else if(input<0)
+      string = string.substring(0,1)+"0"+string.substring(1);
+  }
+  return string.substring(0,string.length()-decimalPlaces)+"."+string.substring(string.length()-decimalPlaces);
+  }
+  else {
+    return String((int)input);
+  }
+}
 
 void sendGpsData(){
   // keep retrying until connected to website
@@ -53,7 +188,8 @@ void sendGpsData(){
 
   // send HTTP request, ends with 2 CR/LF
   Serial.println("send HTTP GET request");
-  c.println("GET http://b4b0a9ed.ngrok.io/gps?lat=1.299155&long=103.787679 HTTP/1.1");
+  Serial.println("lat="+doubleToString(latno,5)+"&long="+doubleToString(longno,5));
+  c.println("GET http://722b4490.ngrok.io/gps?lat="+doubleToString(latno,5)+"&long="+doubleToString(longno,5)+" HTTP/1.1");
   c.println("Host: " SITE_URL);
   c.println("Connection: keep-alive");
   c.println();
@@ -90,6 +226,11 @@ void sendGpsData(){
 
 void loop()
 {
+  /*Get GPS data*/
+  LGPS.getData(&info);
+  Serial.println((char*)info.GPGGA); 
+  parseGPGGA((const char*)info.GPGGA);
+  
   // every 5 seconds
   sendGpsData();
   delay(5000);
