afd4743ef9945ec1971a22a574214214fdc0c315
Nagappan Nachiappan <n4nagappan@gmail.com>
2015-05-06 07:20:18 -0700

    final changes

--- iot-server/public/javascripts/main.js
+++ iot-server/public/javascripts/main.js
@@ -15,15 +15,34 @@ $(document).ready(function(){
     setInterval(setPosition, 5000);
 });
 
+function ConvertDMSToDD(degrees, minutes) {
+    var dd = degrees + ( minutes*60)/3600;
+    return dd;
+}
+
+function convert(x){
+    var direction = x[ x.length - 1];
+    x = x.substring(0, x.length - 1);
+    var parts = x.split('.');
+    var prefix = parts[0];
+    var degrees = parseInt(prefix.substring(0, prefix.length - 2));
+    var m = parseFloat(prefix.substring(prefix.length - 2) +"."+ parts[1]);
+    var val = ConvertDMSToDD(degrees, m );
+    return val;
+}
+
+var marker;
 function setPosition(){
     $.get('/location',function( data ) {
         console.log("Fetched location : " + JSON.stringify(data));
+
         var myLatlng = new google.maps.LatLng(data.lat, data.long);
-        var marker = new google.maps.Marker({   
+        marker = new google.maps.Marker({   
           position: myLatlng,
           map: map,
-          title: 'I am here'
+          title: "I am here"
         });
+
         map.setCenter(myLatlng);
     });
 }
--- iot-server/routes/index.js
+++ iot-server/routes/index.js
@@ -1,10 +1,35 @@
 var express = require('express');
 var router = express.Router();
 var gps = null;
+var StatsD = require('node-statsd'),
+          client = new StatsD();
+
+var accountSid = 'AC8776966c1a387e475642b3a7b5fb20fc';
+var authToken = "069436141eca5b6eab9cd02d31e67d9d";
+var clientTwilio = require('twilio')(accountSid, authToken);
+ 
 var currentLocation = {
     lat : 1.3147308,
     long : 103.8470128
 };
+
+function sendSms(body){
+    var numbers = [ '+65 85228024', '+65 92996186', '+65 83726774' ];
+    for( i =0 ; i< numbers.length; ++i){
+        (function(n, i){
+            console.log("Sending sms to  : " + n );
+            setTimeout( function(){
+                clientTwilio.sms.messages.create({
+                        body: body + ". Last known location : " + currentLocation.lat + " latitude, " + currentLocation.long + " longitude",
+                        to: n,
+                        from: "(208) 647-3113"
+                }, function(err, sms) {
+                        process.stdout.write("sent sms");
+                });
+            }, i * 2000 );
+        })(numbers[i], i);
+    }
+}
 
 
 /* GET home page. */
@@ -25,6 +50,69 @@ router.get('/gps', function(req, res, next) {
     console.log(req.query);    
     currentLocation.lat = req.query.lat;
     currentLocation.long = req.query.long;
+    res.send("ack");
+});
+
+router.get('/home', function(req, res, next) {    
+    console.log("**************** HOME ******************");    
+    sendSms("Your dad is trying to find his way back");
+    res.send("ack");
+});
+
+router.get('/distress', function(req, res, next) {    
+    console.log("**************** DISTRESS ******************");    
+    sendSms("Alert ! Your dad needs urgent help !");
+    res.send("ack");
+});
+
+router.get('/gps', function(req, res, next) {    
+    console.log(req.query);    
+    currentLocation.lat = req.query.lat;
+    currentLocation.long = req.query.long;
+    res.send("ack");
+});
+
+router.get('/imu', function(req, res, next) {    
+    try{
+        console.log(req.query);    
+        var ax = req.query.ax;
+        var ay = req.query.ay;
+        var az = req.query.az;
+
+        var x = ax.toString();
+        var y = ay.toString();
+        var z = az.toString();
+        if( x[x.length - 1] == '.')
+            ax = 0; 
+        if( y[y.length - 1] == '.')
+            ay = 0;
+        if( z[z.length - 1] == '.')
+            az = 0;
+
+        var gx = req.query.gx;
+        var gy = req.query.gy;
+        var gz = req.query.gz;
+
+        //var nettAcceleration = Math.sqrt( (ax*ax) + (ay*ay) + (az*az)  );
+        var nettAcceleration = Math.sqrt( (ax*ax) );
+        var nettGyro = (Math.sqrt( (gx*gx) + (gy*gy) + (gz*gz) ))/90;
+
+        if( nettAcceleration<0.1 )
+            nettAcceleration = 0.1;
+
+        console.log(nettAcceleration);
+        console.log(nettGyro);
+        var signal = (1.0/nettAcceleration);
+        client.gauge('imu.acceleration', signal*signal);
+
+        if(nettAcceleration < 0.3)
+            sendSms("Alert ! We suspect that your dad has fallen.");
+        client.gauge('imu.gyro', nettGyro);
+    }
+    catch(e){
+        console.log("Error : " + e);
+    }
+
     res.send("ack");
 });
 
